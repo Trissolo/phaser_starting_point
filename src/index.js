6c1779f0e0c664ca5c851491b5820316813fbbf8
Trissolo <00tristano00@gmail.com>
2017-09-04 18:00:20 +0200

    Inizio

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/**
+ * Import Phaser dependencies using `expose-loader`.
+ * This makes then available globally and it's something required by Phaser.
+ * The order matters since Phaser needs them available before it is imported.
+ */
+
+import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
+import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
+import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
+
+/**
+ * Create a new Phaser game instance.
+ * And render a single sprite so we make sure it works.
+ */
+
+window.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+  game.load.image('logo', './assets/images/phaser.png');
+}
+
+function create() {
+  this.logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+  this.logo.anchor.setTo(0.5, 0.5);
+};
+  
+function update() {
+  // ¯ \_(ツ)_/¯ 
+  // "surprise me"
+}
+
