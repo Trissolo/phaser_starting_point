ca1e526a127d54b1faf5af2803b26ba53012dbdc
Trissolo <00tristano00@gmail.com>
2017-09-05 08:32:10 +0200

    Fixed babel-loader configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+//const webpack = require('webpack');
 
 module.exports = {
 
@@ -8,6 +9,12 @@ module.exports = {
    */
 
   entry: './src/index.js',
+/*
+	entry: {
+	app: ['babel-loader',
+      path.resolve(__dirname, 'public/assets/scripts/bundle.js')]
+	},*/
+
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public', 'assets', 'scripts')
@@ -20,10 +27,10 @@ module.exports = {
    */
 
   devServer: {
-    publicPath: '/assets/scripts/',
-    contentBase: path.join(__dirname, 'public'),
-    port: 8000,
-	open: true
+		publicPath: '/assets/scripts/',
+		contentBase: path.join(__dirname, 'public'),
+		port: 8000,
+		open: true
   },
 
 	/**
@@ -32,19 +39,22 @@ module.exports = {
 	* 
 	*/
 	module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-			include: path.resolve(__dirname, "public/assets"),
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    }
-  ]
-}
+		rules: [
+		  {
+		    test: /\.js$/,
+		    //exclude: /(node_modules|bower_components)/,
+				exclude:path.resolve("./node_modules"),
+				include: path.join(__dirname, "src"),///assets/scripts"),
+				//include: path.resolve("./public/assets/scripts"),
+		    use: {
+		      loader: 'babel-loader',
+		      options: {
+		        presets: ['env']
+		      }
+		    }
+		  }
+		]
+	},
+
 };
 
